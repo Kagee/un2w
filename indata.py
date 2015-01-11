d6b105adef1b6f20660363c3805092c3c868606a
Anders Einar Hilden <hildenae@gmail.com>
2015-01-11 16:55:58 +0100

    Yes.

--- indata.py
+++ indata.py
@@ -3,7 +3,8 @@ import sys
 import pynmea2
 import time
 from geojson import FeatureCollection, Feature, LineString
-
+from collections import deque
+import os.path
 #CONFIG
 USERS=[("hildenae","htconem8"),("hildenæ","htcone m8!")]
 
@@ -13,6 +14,8 @@ UDP_PORT = 11109
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+
+l = deque([], 1000)
 
 run = True
 while run:
@@ -25,15 +28,25 @@ while run:
         acc, dev, nmea = s.split('/')
         msg = pynmea2.parse(nmea)
         print (msg.timestamp, msg.latitude, msg.longitude)
-        from geojson import LineString
-        ls = LineString([(float("%.6f" % msg.latitude), float("%.6f" % msg.longitude))])
+        t = (float("%.6f" % msg.latitude), float("%.6f" % msg.longitude))
+        ls = LineString([t])
+        l.append(t)
         print (ls)
+        #print (FeatureCollection(Feature(LineString(list(l)))))
+        fn = "%s-%s.json" % (acc, dev)
+        #if (os.path.isfile(fn)):
+        #    print(fn, "exsists")
+        #else:
+        #    print(fn, "dontnt exsists")
+        f = open(fn, 'w')
+        print (FeatureCollection([Feature("line1", LineString(list(l)))]), file=f)
+        f.close()
         sys.stdout.flush()
     except UnicodeError as ue:
         print("Discarding, failed to decode as UTF-8: %s (%s)" % (data, ue), file=sys.stderr)
         continue
     except ValueError as ve:
-        print (ve, file=sys.stderr)
+        print ("%s: (%s)" %(ve, s), file=sys.stderr)
         continue
     except pynmea2.nmea.ParseError:
         print("Discarding, failed to parse NMEA: ", s, file=sys.stderr)
